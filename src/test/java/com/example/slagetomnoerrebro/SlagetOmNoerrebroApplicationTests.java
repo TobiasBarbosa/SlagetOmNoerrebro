d1e053a501701509506d07ecd79d63f7e9cd4c07
“TobiasBarbosa” <toha0006@stud.kea.dk>
2024-10-02 10:02:03 +0200

    Initial commit

---
+++ src/test/java/com/example/slagetomnoerrebro/SlagetOmNoerrebroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slagetomnoerrebro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlagetOmNoerrebroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
